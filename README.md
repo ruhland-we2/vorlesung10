7da5d770e1003ad0a89b5edffb01aad3abd85ea9
hszg-ruhland <kruhland@hszg.de>
2022-06-16 13:11:41 +0200

    debug

--- README.md
+++ README.md
@@ -32,3 +32,74 @@ Mit dem Kommando
 $npx tsc --init
 ````
 wird eine Datei *tsconfig.json* als Konfigurationsdatei für TypeScript generiert.
+
+## require in NodeJS vs. import in TypeScript
+
+Als erstes werden die 
+
+```
+const express = require('express');
+const helmet = require('helmet');
+const bs58 = require('bs58');
+```
+
+umgeschrieben in
+
+```
+import express from 'express';
+import helmet from 'helmet';
+import bs58 from 'bs58';
+```
+
+Mit dem Kommando 
+
+```
+$npx tsc
+```
+
+werden die Dateien - hier nur restserver.ts - compiliert.
+Es wird ein Fehler
+```
+src/restserver.ts:69:62 - error TS2554: Expected 0-1 arguments, but got 2.
+69 app.post('*', function(req, res){ res.send('file not found', 404); });
+```
+angezeigt, der in JavaScript nicht ersichtlich war, weil er aus einer alten Version von express stammt.
+
+Damit die compilierte restserver.js Datei im dist Verzeichnis liegt muss man in der tsconfig.json die Zeile
+
+```
+    "outDir": "./dist",                                   /* Specify an output folder for all emitted files. */
+```
+auskommentiert und geändert werden.
+
+## Konfiguration von NodeJS
+
+In der Datei *package.json* wird der Abschnitt *scripts* geändert in
+
+```
+  "scripts": {
+    "compile": "tsc"
+  },
+```
+
+so dass mit dem Kommando
+
+```
+$npm run compile
+```
+
+die Typescript Compilierung angestoßen wird
+
+## Debuggen von TypeScript
+
+Zu diesem Zweck muss in der tsconfig.json der Eintrag
+
+```
+    "sourceMap": true,                                /* Create source map files for emitted JavaScript files. */
+```
+
+gesetzt werden. Danach kann man in Visual Studio Code den restserver.ts debuggen.
+
+
+
+
--- dist/restserver.js
+++ dist/restserver.js
@@ -1,74 +1,68 @@
 #!/usr/bin/env nodejs
-/* restserver.js 
+"use strict";
+/* restserver.js
  * npm i express
  * npm i helmet
  * npm i bs58
  * npm i -D nodemon
  * run for development with: $npx nodemon restserver.js
- * 
+ *
  * NodeJS Restful Server, 03-2021
  * Klaus Ruhland, HSZG, Betriebssysteme II, Web Engineering II
  * Test with curl
  * 1.) curl  "http://localhost:8000/service/users?id=4&token=sdfa3&geo=us"
  * 2.) curl http://localhost:8000/service/test/12345
  * 3.) curl -d "param1=value1&param2=value2" -X POST http://localhost:8000/service/users
- * 
+ *
  * 2.) mit Base58 encoding
  * curl http://localhost:8000/service/test-b58/79jdpA
  * Base58 online encoding with https://appdevtools.com/base58-encoder-decoder
  */
-
-
-import express from 'express';
-import helmet from 'helmet';
-import bs58 from 'bs58';
-
-const app = express();
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const helmet_1 = __importDefault(require("helmet"));
+const bs58_1 = __importDefault(require("bs58"));
+const app = (0, express_1.default)();
 const port = process.env.PORT || 8000;
-
 // initializations
-app.use(helmet());
-
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
+app.use((0, helmet_1.default)());
+app.use(express_1.default.urlencoded({ extended: true }));
+app.use(express_1.default.json());
 // Routing Examples
 // http GET: http://localhost:8000/service/users?id=4&token=sdfa3&geo=us
 // Paremeter through ? in url 
-app.get('/service/users', function(req, res) {
+app.get('/service/users', function (req, res) {
     res.json(req.query);
 });
 // http GET: http://localhost:8000/service/test/12345
 // Parameter through path
-app.get('/service/test/:version', function(req, res) {
+app.get('/service/test/:version', function (req, res) {
     res.json(req.params);
 });
-
 // http GET: curl http://localhost:8000/service/test-b58/79jdpA
 // Parameter through path with base58 decoding
 // 79jdpA is a base58 encoded 😏
-app.get('/service/test-b58/:b58version', function(req, res) {
+app.get('/service/test-b58/:b58version', function (req, res) {
     let b58versionstring = req.params.b58version;
-    const bytes = bs58.decode(b58versionstring);
+    const bytes = bs58_1.default.decode(b58versionstring);
     const version = Buffer.from(bytes).toString("utf8");
-    
     let result = {
         version: version
     };
-
     res.json(result);
 });
-
 // http POST: http://localhost:8000/service/users
-app.post('/service/users', function(req, res) {
+app.post('/service/users', function (req, res) {
     res.json(req.body);
 });
-
 //The 404 Route (ALWAYS Keep this as the last route)
-app.get('*', function(req, res){ res.status(404).send('file not found'); });
-app.post('*', function(req, res){ res.status(404).send('file not found'); });
-
+app.get('*', function (req, res) { res.status(404).send('file not found'); });
+app.post('*', function (req, res) { res.status(404).send('file not found'); });
 // Running the server
 app.listen(port, () => {
-      console.log(`http://localhost:${port}`);
-})
+    console.log(`http://localhost:${port}`);
+});
+//# sourceMappingURL=restserver.js.map
